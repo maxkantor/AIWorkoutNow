1eb867e9b98d2b90139572513efa5b818c90d869
Max Kantor <mykantor@bellsouth.net>
2026-01-14 20:11:50 -0500

    SEO: set OG/Twitter image to /images/og-image.png

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -204,19 +204,19 @@ function Home() {
         <link rel="canonical" href="https://aiworkoutnow.com/" />
 
         {/* Open Graph / Twitter */}
-        <meta property="og:title" content={seoTitle} />
-        <meta property="og:description" content={seoDescription} />
-        <meta property="og:url" content="https://aiworkoutnow.com/" />
+        <meta property="og:title" content="AIWorkoutNow" />
+        <meta property="og:description" content="Get Your Perfect Workout in Seconds" />
+        <meta property="og:url" content="https://AIWorkoutNow.com/" />
         <meta property="og:type" content="website" />
-        <meta property="og:image" content="https://aiworkoutnow.com/og-image.svg" />
+        <meta property="og:image" content="https://AIWorkoutNow.com/images/og-image.png" />
         <meta property="og:image:width" content="1200" />
         <meta property="og:image:height" content="630" />
         <meta property="og:site_name" content="AIWorkoutNow" />
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:url" content="https://aiworkoutnow.com/" />
-        <meta name="twitter:title" content={seoTitle} />
-        <meta name="twitter:description" content={seoDescription} />
-        <meta name="twitter:image" content="https://aiworkoutnow.com/og-image.svg" />
+        <meta name="twitter:url" content="https://AIWorkoutNow.com/" />
+        <meta name="twitter:title" content="AIWorkoutNow" />
+        <meta name="twitter:description" content="Get Your Perfect Workout in Seconds" />
+        <meta name="twitter:image" content="https://AIWorkoutNow.com/images/og-image.png" />
 
         {/* Structured data */}
         <script type="application/ld+json">
